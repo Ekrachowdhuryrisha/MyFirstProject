5b2f41072cc0c8761b722086d208f5f1d4365cb1
Ekra Chowdhury Risha <135812254+Ekrachowdhuryrisha@users.noreply.github.com>
2024-01-04 20:52:22 +0600

    Add files via upload

---
+++ ALL_IMPORTANT_INFO_FOR_MY_SECTION'S_STUDENT.cpp
@@ -0,0 +1,175 @@
+/*Author-Ekra Chowdhury Risha
+student, CSE, University of Asia Pacific.
+**/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<" "<<endl;
+cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+cout<<" "<<endl;
+cout<<"      ^          /`\\   |`|   |`|        |````| |\\  /| |```| |```| |```| |````|  /`\\   |`\\|| |````|    |````| |`\\|| ||``` |```|        ^   "<<endl;
+cout<<"   ( o_o )      / - \\  | |__ | |__        ||   ||\\/|| | __| |   | | __|   ||   / - \\  ||\\\\|   ||        ||   ||\\\\| ||``  |   |     ( o_o )" <<endl;
+cout<<"               / / \\ \\ |____||____|     |____| ||  || ||    |___| ||\\\\    ||  / / \\ \\ || \\|   ||      |____| || \\| ||    |___|           "<<endl;
+cout<<" "<<endl;
+cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+cout<<" "<<endl;
+cout<<" "<<endl;
+cout<<"                                               ``````````````````````````````````````````````                                                 "<<endl;
+cout<<"                                                      WHAT DO YOU WANT TO KNOW?  >o^o<                                                        "<<endl;
+cout<<"                                               ``````````````````````````````````````````````                                                 "<<endl;
+cout<<" "<<endl;
+cout<<"                                               1.Course Details of 2-1                                                                        "<<endl;
+cout<<"                                               2.Classroom Links                                                                              "<<endl;
+cout<<"                                               3.Previous(2-1,spring-23) Course Material                                                      "<<endl;
+cout<<"                                               4.Class Routine                                                                                "<<endl;
+cout<<"                                               5.Advisor's Details                                                                            "<<endl;
+cout<<"                                               6.All Course Teacher's Details                                                                 "<<endl;
+cout<<" "<<endl;
+cout<<" "<<endl;
+cout<<" "<<endl;
+cout<<"              ( >o_o< )  Enter Any Number(1-6) According to Your Choice:";
+int n;
+cin>>n;
+char c;
+if(n==1){
+        cout<<" "<<endl;
+cout<<"                                   1.CSE 201 -> Object-Oriented Programming I                                                         "<<endl;
+cout<<"                                   2.CSE 202 -> Object-Oriented Programming I Lab                                                     "<<endl;
+cout<<"                                   3.CSE 203 -> Data Structures and Algorithms I                                                      "<<endl;
+cout<<"                                   4.CSE 204 -> Data Structures and Algorithms I Lab                                                  "<<endl;
+cout<<"                                   5.CSE 205 -> ICT Law, Policy and Ethics                                                            "<<endl;
+cout<<"                                   6.MTH 201 -> Vector Geometry and Linear Algebra                                                    "<<endl;
+cout<<"                                   7.EEE 201 -> Electronic Devices and Circuit                                                        "<<endl;
+cout<<"                                   8.EEE 202 -> Electronic Devices and Circuit Lab                                                    "<<endl;
+cout<<" "<<endl;
+cout<<" "<<endl;
+cout<<"                                   (o^o)  Thanks For Using This Program  (o^o)                                                                               "<<endl;
+cout<<"   "<<endl;
+cout<<"         Enter c/C to Close the Program:";
+cin>>c;
+if(c=='c' || 'C'){
+    return 0;
+}
+}
+
+if(n==2){
+cout<<" "<<endl;
+cout<<"  "<<endl;
+cout<<"                           Google Classromm Link Will be Added Later    >o_o<                                    "<<endl;
+cout<<" "<<endl;
+cout<<" "<<endl;
+cout<<"                           (o^o)  Thanks For Using This Program  (o^o)                                                                               "<<endl;
+cout<<"   "<<endl;
+cout<<"       Enter c/C to Close the Program:";
+cin>>c;
+if(c=='c' || 'C'){
+    return 0;
+}
+}
+
+if(n==3){
+cout<<" "<<endl;
+cout<<" "<<endl;
+cout<<"                           Google Drive Link:  https://drive.google.com/drive/folders/1WWs23cTCY5eLhArn8ejF95DC-jmSADsc?usp=sharing               "<<endl;
+cout<<" "<<endl;
+cout<<"                           NOTE: YOU HAVE TO COPY THE LINK FROM HERE AND PASTE IN BROWSER.                                             "<<endl;
+cout<<" "<<endl;
+cout<<" "<<endl;
+cout<<"                           (o^o)  Thanks For Using This Program  (o^o)                                                                               "<<endl;
+cout<<"   "<<endl;
+cout<<"       Enter c/C to Close the Program:";
+cin>>c;
+if(c=='c' || 'C'){
+    return 0;
+}
+}
+if(n==4){
+cout<<" "<<endl;
+cout<<" "<<endl;
+cout<<"                           Class Routine Will be Added Later   >o_o<                         "<<endl;
+cout<<" "<<endl;
+cout<<" "<<endl;
+cout<<"                           (o^o)  Thanks For Using This Program  (o^o)                                                                               "<<endl;
+cout<<"   "<<endl;
+cout<<"       Enter c/C to Close the Program:";
+cin>>c;
+if(c=='c' || 'C'){
+    return 0;
+}
+}
+if(n==5){
+cout<<" "<<endl;
+cout<<"                           Advisor's Details:                         "<<endl;
+cout<<"                           Nuzhat Tabassum Progga,                    "<<endl;
+cout<<"                           Lecturer, University of Asia Pacific.      "<<endl;
+cout<<"                           Phone: 01521115015.                        "<<endl;
+cout<<"                           E-mail: progga@uap-bd.edu                  "<<endl;
+cout<<" "<<endl;
+cout<<" "<<endl;
+cout<<"                           (o^o)  Thanks For Using This Program  (o^o)                                                                               "<<endl;
+cout<<"   "<<endl;
+cout<<"       Enter c/C to Close the Program:";
+cin>>c;
+if(c=='c' || 'C'){
+    return 0;
+}
+}
+
+if(n==6){
+cout<<" "<<endl;
+cout<<" "<<endl;
+cout<<"                           Course Code: CSE-201.                      "<<endl;
+cout<<"                           Course Teacher Name:_________              "<<endl;
+cout<<"                           Phone:-----------                          "<<endl;
+cout<<"                           E-mail:----------                          "<<endl;
+cout<<" "<<endl;
+cout<<"                           Course Code: CSE-202.                      "<<endl;
+cout<<"                           Course Teacher Name:_________              "<<endl;
+cout<<"                           Phone:-----------                          "<<endl;
+cout<<"                           E-mail:----------                          "<<endl;
+cout<<" "<<endl;
+cout<<"                           Course Code: CSE-203.                      "<<endl;
+cout<<"                           Course Teacher Name:_________              "<<endl;
+cout<<"                           Phone:-----------                          "<<endl;
+cout<<"                           E-mail:----------                          "<<endl;
+cout<<" "<<endl;
+cout<<"                           Course Code: CSE-204.                      "<<endl;
+cout<<"                           Course Teacher Name:_________              "<<endl;
+cout<<"                           Phone:-----------                          "<<endl;
+cout<<"                           E-mail:----------                          "<<endl;
+cout<<" "<<endl;
+cout<<"                           Course Code: CSE-205.                      "<<endl;
+cout<<"                           Course Teacher Name:_________              "<<endl;
+cout<<"                           Phone:-----------                          "<<endl;
+cout<<"                           E-mail:----------                          "<<endl;
+cout<<" "<<endl;
+cout<<"                           Course Code: MTH-201.                      "<<endl;
+cout<<"                           Course Teacher Name:_________              "<<endl;
+cout<<"                           Phone:-----------                          "<<endl;
+cout<<"                           E-mail:----------                          "<<endl;
+cout<<" "<<endl;
+cout<<"                           Course Code: EEE-201.                      "<<endl;
+cout<<"                           Course Teacher Name:_________              "<<endl;
+cout<<"                           Phone:-----------                          "<<endl;
+cout<<"                           E-mail:----------                          "<<endl;
+cout<<" "<<endl;
+cout<<"                           Course Code: EEE-202.                      "<<endl;
+cout<<"                           Course Teacher Name:_________              "<<endl;
+cout<<"                           Phone:-----------                          "<<endl;
+cout<<"                           E-mail:----------                          "<<endl;
+cout<<" "<<endl;
+cout<<"                           NOTE: ALL INFORMATION WILL BE ADDED AFTER GETTING THE CLASS ROUTINE.        "<<endl;
+cout<<" "<<endl;
+cout<<" "<<endl;
+cout<<"                           (o^o)  Thanks For Using This Program  (o^o)                                                                               "<<endl;
+cout<<"   "<<endl;
+cout<<"       Enter c/C to Close the Program:";
+cin>>c;
+if(c=='c' || 'C'){
+    return 0;
+}
+}
+return 0;
+}
